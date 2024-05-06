05591d65f309a21452235630959dd33b729b4f7a
William Brown <140029632+brogment@users.noreply.github.com>
2024-05-06 14:43:17 -0500

    Finish highlight header section in view or clicked

--- components/about.tsx
+++ components/about.tsx
@@ -2,14 +2,20 @@
 
 import React from 'react'
 import SectionHeading from './section-heading'
-import { animate, motion } from "framer-motion";
+import { motion } from "framer-motion";
+import { useSectionInView } from '@/lib/hooks';
 
 export default function About() {
+  const { ref } = useSectionInView('About');
   return (
-    <motion.section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'
-        initial={{ opacity: 0, y:100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.175}}>
+    <motion.section 
+      ref = {ref}
+      className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28'
+      initial={{ opacity: 0, y:100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175}}
+      id='about'
+    >
         <SectionHeading>About Me</SectionHeading>
         <p className='mb-3'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
